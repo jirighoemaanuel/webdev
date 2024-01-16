ee3152592ab4aabd10fc2ae2957d2237af7fab7f
jirighoemaanuel <emmaweyinmi@gmail.com>
2024-01-16 22:03:32 +0100

    d

--- fastapi/main.py
+++ fastapi/main.py
@@ -23,8 +23,18 @@ def create_employees():
 
     return jsonify(employee), 201
 
-    
-    
+
+##write the code to handle update request
+# @app.route('/employees', methods=['POST'])
+# def create_employees():
+#     global nextEmployeeId
+#     employee = json.loads(request.data)
+
+#     nextEmployeeId += 1
+#     employee['id'] = nextEmployeeId
+#     employees.append(employee)
+
+#     return jsonify(employee), 201
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
